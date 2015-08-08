c4bbe71e8916f69a94c61f27a4cba72b5d0fd278
Silas Barta <sbarta@gmail.com>
2015-08-08 11:55:04 -0700

    Initial commit of todo app.

---
+++ www/js/app.js
@@ -0,0 +1,56 @@
+// Ionic Starter App
+
+angular.module('todo', ['ionic'])
+.controller('TodoCtrl', function($scope, $ionicModal) {
+  $scope.tasks = [
+    { title: 'Collect coins' },
+    { title: 'Eat mushrooms' },
+    { title: 'Get high enough to grab the flag' },
+    { title: 'Find the Princess' }
+  ];
+  // Create and load the Modal
+  $ionicModal.fromTemplateUrl('new-task.html', function(modal) {
+    $scope.taskModal = modal;
+  }, {
+    scope: $scope,
+    animation: 'slide-in-up'
+  });
+
+  // Called when the form is submitted
+  $scope.createTask = function(task) {
+    $scope.tasks.push({
+      title: task.title
+    });
+    $scope.taskModal.hide();
+    task.title = "";
+  };
+
+  $scope.deleteTask = function(task) {
+      console.log(task.title);
+      $scope.tasks = $scope.tasks.filter(function(el) {
+          return el !== task;
+      });
+  };
+
+  // Open our new task modal
+  $scope.newTask = function() {
+    $scope.taskModal.show();
+  };
+
+  // Close the new task modal
+  $scope.closeNewTask = function() {
+    $scope.taskModal.hide();
+  };
+});
+//.run(function($ionicPlatform) {
+//  $ionicPlatform.ready(function() {
+//    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+//    // for form inputs)
+//    if(window.cordova && window.cordova.plugins.Keyboard) {
+//      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+//    }
+//    if(window.StatusBar) {
+//      StatusBar.styleDefault();
+//    }
+//  });
+//})
